8577df9e39a3ac6344e5d97aa6e5634a9c399448
ik3nna <iamiykendukwu@gmail.com>
2024-05-13 15:05:33 +0100

    feat: Initial setup

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+// security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+const express = require('express');
+const app = express();
+
+app.set('truxt proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}));
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+    //   await connectDB(process.env.MONGO_URI)
+      app.listen(port, () =>
+        console.log(`Server is listening on port ${port}...`)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  start();
