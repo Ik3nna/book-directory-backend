69f715962c34c9e4b43b2f1e7511b0ec05d08419
ik3nna <iamiykendukwu@gmail.com>
2024-05-20 13:17:15 +0100

    feat: Book controllers

--- app.js
+++ app.js
@@ -32,14 +32,14 @@ app.use('/api/v1/books', booksRouter);
 const port = process.env.PORT || 5000;
 
 const start = async () => {
-    try {
-      await connectDB(process.env.MONGO_URI)
-      app.listen(port, () =>
-        console.log(`Server is listening on port ${port}...`)
-      );
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
   
 start();
--- controllers/book.js
+++ controllers/book.js
@@ -2,23 +2,50 @@ const Book = require('../models/book');
 const { StatusCodes } = require('http-status-codes')
 
 const getAllBooks = async (req, res) => {
-
+    const books = await Book.find({});
+    res.status(StatusCodes.OK).json({ books })
 }
 
 const getSingleBook = async (req, res) => {
+    const { id } = req.params;
+    const book = await Book.findOne({_id: id});
 
+    if (!book) {
+
+    }
+
+    res.status(StatusCodes.OK).json({ book })
 }
 
 const addBook = async (req, res) => {
-
+    const newBook = await Book.create(req.body);
+    res.status(StatusCodes.CREATED).json({ newBook })
 }
 
 const updateBook = async (req, res) => {
+    const { id } = req.params;
+    const book = await Book.findByIdAndUpdate(
+        {_id: id},
+        req.body,
+        { new: true, runValidators: true }
+    )
 
+    if (!book) {
+
+    }
+
+    res.status(StatusCodes.OK).json({book})
 }
 
 const deleteBook = async (req, res) => {
+    const { id } = req.body;
+    const book = await Book.findOneAndDelete({_id:id});
 
+    if (!book) {
+
+    }
+    
+    res.status(StatusCodes.OK).json({ book })
 }
 
 module.exports = {
